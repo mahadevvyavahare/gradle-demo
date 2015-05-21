f412b1874e6b3fcd1a1eef980a48f2750f639fe6
gopinathlangote <gopinathlangote11@gmail.com>
2015-05-21 20:47:55 +0530

    replaced code

---
+++ src/main/java/help/Helper.java
@@ -0,0 +1,20 @@
+package help;
+
+public class Helper {
+
+    public static int maximum(int a,int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+    public static int minimum(int a,int b) {
+        if(a<b) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+}
